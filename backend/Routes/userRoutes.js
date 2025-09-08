91332a27ef8874787bba6a6b15bee558f3755485
IsSurajKadam <surajkadam1706004@gmail.com>
2025-09-08 17:15:07 +0530

    Update userRoutes.js

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { register,login,logout,getUser,updatePassword,updateProfile,googleAuth} from '../controllers/userController.js'; 
+import { register,login,logout,getUser,updatePassword,updateProfile,googleAuth,checkMobile} from '../controllers/userController.js'; 
 import { isAuthenticated, isAuthorized } from "../middleware/auth.js";
 const router =express.Router();
 router.post('/register',register);
 router.post("/login",login);
 router.post("/logout",logout);
 router.post("/auth/google",googleAuth);
+router.get("/check-mobile",checkMobile);
 
 
 router.get("/get-me",isAuthenticated,getUser);
